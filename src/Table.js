efeb4a1b18aeaa31138635844d6248808854c24c
Storm Randolph <storm.randolph@gmail.com>
2023-01-17 17:04:39 -0800

    finished react app part II

--- src/Table.js
+++ src/Table.js
@@ -15,12 +15,12 @@ function TableHeader()  {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-  <td>{row.name}</td>
-  <td>{row.job}</td>
-  <td>
-    <button onClick={() => props.removeCharacter(index)}>Delete</button>
-  </td>
-</tr>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
+        </tr>
       );
      }
     );
