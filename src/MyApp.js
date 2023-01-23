b35b77f60cc19493de57fc929eff11bc989be766
Storm Randolph <storm.randolph@gmail.com>
2023-01-22 20:03:04 -0800

    finished part 1

--- src/MyApp.js
+++ src/MyApp.js
@@ -21,6 +21,19 @@ function removeOneCharacter (index) {
      });
  }, [] );
 
+ async function makePostCall(person){
+  try {
+     const response = await axios.post('http://localhost:5000/users', person);
+     return response;
+  }
+  catch (error) {
+     console.log(error);
+     return false;
+  }
+}
+
+
+
   async function fetchAll(){
     try {
        const response = await axios.get('http://localhost:5000/users');
@@ -33,10 +46,13 @@ function removeOneCharacter (index) {
     }
  }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
-
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 200)
+       setCharacters([...characters, person] );
+    });
+ }
+ 
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
